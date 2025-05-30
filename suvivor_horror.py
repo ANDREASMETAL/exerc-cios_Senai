a995557e6a6f628556ebea8011e6c56ad9ccfb7a
ANDREASMETAL <andreas.ribeiro.f@gmail.com>
2025-05-30 11:52:49 -0300

    Add files via upload

---
+++ suvivor_horror.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+# ---------------------------------------------- SIDEBAR
+
+st.sidebar.title('consoles')
+st.sidebar.title('Datas')
+
+
+
+
+
+# ---------------------------------------------- PRINCIPAL
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['playstation one', 'Sega saturn', 'Nintendo Gamecub', 'Dreamcast','Playstation2'])
+
+with tab1:
+    st.title('Jogos De Suvivor Horror')
+    st.subheader('Os principais:')
+    st.write('''
+ - Resident Evil 1
+ - Resident Evil 2
+ - Resident Evil 3
+ - Silent Hill
+ - Alone In The Dark New Nigthmare
+- Nigthmare Criatures''')
+with tab2:
+    st.title('Jogos De Suvivor Horror')
+    st.subheader('Os principais:')
+    st.write ('''
+- Resident Evil 1
+- Enemy Zero
+- The House Of Dead
+- Alone In The Dark 2''')
+with tab3:
+    st.title('Jogos De Suvivor Horror')
+    st.subheader('Os principais:')
+    st.write ('''
+- Resident Evil 0
+- Resident Evil Remake
+- Resident Evil 2
+- Resident Evil 3
+- Resident Evil 4''')
