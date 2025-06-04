65000f9814e138ceaee9a15d321db9743734b8ab
ANDREASMETAL <andreas.ribeiro.f@gmail.com>
2025-06-04 11:28:36 -0300

    Add files via upload

--- media_prova.py
+++ media_prova.py
@@ -1,4 +1,14 @@
-print('Ola, por favor imforme a quantia de provas que o aluno realizou.')
-provas= int(input)
-print(provas)
-media= provas//2
+print('Ola, seja bem vindo.')
+provas= int(input('Por favor imforme a quantia de provas que o aluno realizou: '))
+contador=1
+soma=0
+
+while contador <= provas:
+    print('|',30*'-','|')
+    nota = float(input(f'Digite a nota da prova {contador}: '))
+    soma = soma+ nota
+    contador = contador+1
+    
+media= soma/ provas
+print('|',30*'-','|')
+print(f'A media do aluno é {media:.2f}')
